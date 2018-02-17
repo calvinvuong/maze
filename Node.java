cfda04d8b9fa596a265853a0888a0c8705b19c73
Steven Chen Hao Nyeo <limpingstone@Lstone529-PCA.slackware.lab>
2018-02-17 12:19:37 -0500

    Maze node v.1

--- Node.java
+++ Node.java
@@ -1,3 +1,47 @@
-public class Node {
 
+public class Node {
+	
+	private Node[] nextNodes = new Node[3]; 
+	private int link;
+	private int IDNumber;
+
+	public Node(Node nextLeft, Node nextForward, Node nextRight, int link, int idNumber) {
+		getNextNodes()[0] = nextLeft;
+		getNextNodes()[1] = nextForward;
+		getNextNodes()[2] = nextRight;
+		setLink(link);
+		setIDNumber(idNumber);
+	}
+	
+	public Node(int link, int idNumber) {
+		for (int i = 0; i < 3; i++)
+			getNextNodes()[i] = null;
+		setLink(link);
+		setIDNumber(idNumber);
+	}
+	
+	public Node[] getNextNodes() {
+		return nextNodes;
+	}
+	
+	public void setNextNodes(Node[] nextNodes) {
+		this.nextNodes = nextNodes;
+	}
+
+	public int getLink() {
+		return link;
+	}
+
+	public void setLink(int link) {
+		this.link = link;
+	}
+
+	public int getIDNumber() {
+		return IDNumber;
+	}
+
+	public void setIDNumber(int iDNumber) {
+		IDNumber = iDNumber;
+	}
+	
 }
